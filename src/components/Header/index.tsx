2a222a448a60af0a34922f4d0d5cfc792a705121
Isaac-Yuri <isaacyuri22@gmail.com>
2022-07-04 20:48:01 -0300

    componente Header adicionado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer, TitleHeader } from "./styles";
+
+export const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <TitleHeader>Lista de tarefas</TitleHeader>
+    </HeaderContainer>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+import { themes } from "../../themes";
+
+export const HeaderContainer = styled.View`
+  background-color: ${themes.colors.colorApplication};
+  padding: 10px 0;
+`;
+
+export const TitleHeader = styled.Text`
+  color: #fff;
+  text-align: center;
+  font-size: 1em;
+`;
