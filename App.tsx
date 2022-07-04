7acfa1ced6e59baf6feacb32d89344e5cc871c29
Isaac-Yuri <isaacyuri22@gmail.com>
2022-07-04 20:16:49 -0300

    cor padrao da aplicacao adicionado

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Home } from "./src/pages/Home";
+
+import { themes } from "./src/themes";
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Text>App de lista de tarefas!</Text>
-      <StatusBar style="auto" />
+      <Home />
+      <StatusBar
+        backgroundColor={`${themes.colors.colorApplication}`}
+        style="auto"
+      />
     </SafeAreaView>
   );
 }
---
+++ src/themes/index.ts
@@ -0,0 +1,5 @@
+export const themes = {
+  colors: {
+    colorApplication: "#ea4965",
+  },
+};
