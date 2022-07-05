086df5fa410a1d6d6becf5cf8c4e50c9a8ea6a46
Isaac-Yuri <isaacyuri22@gmail.com>
2022-07-05 10:20:31 -0300

    comeco da implementacao das tarefas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { TouchableOpacity, View, Text } from "react-native";
 
 import { Header } from "../../components/Header";
 import { InputTask } from "../../components/InputTask";
@@ -7,22 +6,30 @@ import { AddButton } from "../../components/AddButton";
 
 import { ContainerApplication, BoxInputAndAdd } from "./styles";
 
+import { ITask } from "../../types";
+
 export const Home = () => {
   const [inputData, setInputData] = useState<string>();
+  const [tasks, setTasks] = useState<ITask[]>([
+    { title: "Estudar", isComplete: true },
+    { title: "Treinar saltos", isComplete: false },
+    { title: "Ler", isComplete: false },
+    { title: "Tomar café", isComplete: true },
+  ]);
 
   return (
-    <View>
+    <>
       <Header />
       <ContainerApplication>
         <BoxInputAndAdd>
           <InputTask
             value={inputData}
             onChangeText={(text) => setInputData(text)}
-            placeholder='Digite sua tarefa aqui'
+            placeholder="Digite sua tarefa aqui"
           />
           <AddButton>Add</AddButton>
         </BoxInputAndAdd>
       </ContainerApplication>
-    </View>
+    </>
   );
 };
---
+++ src/types/index.d.ts
@@ -0,0 +1,4 @@
+export interface ITask {
+  title: string;
+  isComplete: boolean;
+}
